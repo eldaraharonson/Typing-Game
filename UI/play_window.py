5361707633af701d4c2bf6d8fd2b815f90643fc9
Eldar Aharonson <eaharonson1806@gmail.com>
2021-05-13 16:26:15 +0300

    seperated contentes to different files

--- UI/play_window.py
+++ UI/play_window.py
@@ -1,5 +1,62 @@
 from tkinter import *
+import keyboard
+
+letter_of_index = 0
 
 
 def initialize_play_window():
     play_window = Tk()
+    define_window_style(play_window)
+    list_of_letter_labels = add_text_for_typing(play_window)
+    config_typing_actions(play_window, list_of_letter_labels)
+    play_window.mainloop()
+
+
+def define_window_style(play_window):
+    play_window.rowconfigure((0, 1, 2, 3, 4), weight=1)
+    play_window.columnconfigure((0, 1, 2, 3, 4), weight=1)
+    play_window.title("Type Master")
+    play_window.geometry("1000x500")
+    play_window["bg"] = "light blue"
+    Label(play_window, text="Type!", bg="light blue", font=("Helvetica", 30)).place(x=440, y=0)
+
+
+def config_typing_actions(play_window, list_of_letter_labels):
+    play_window.bind("<KeyPress-Shift_L>", lambda event: analyze_shift_press(event, list_of_letter_labels))
+    play_window.bind("<KeyPress-Shift_R>", lambda event: analyze_shift_press(event, list_of_letter_labels))
+    play_window.bind("<Key>", lambda event: analyze_key_press(event, list_of_letter_labels))
+
+
+def analyze_shift_press(event, letter_labels):
+    while keyboard.is_pressed("shift"):
+        if keyboard.read_key():
+            break
+
+
+def analyze_key_press(event, letter_labels):
+    global letter_of_index
+    if event.char == letter_labels[letter_of_index].cget("text"):
+        letter_labels[letter_of_index]["bg"] = "lawn green"
+    else:
+        letter_labels[letter_of_index]["bg"] = "tomato"
+    letter_of_index += 1
+
+
+def add_text_for_typing(play_window):
+    frame = Frame(play_window, width=950, height=430, bg="light blue")
+    frame.grid(row=1, column=0)
+    text = "Hello my name is Eldar, and this is my typing game. I hope you enjoy it."
+    list_of_letter_labels = []
+    for letter in text:
+        label_of_letter = Label(frame, text=letter, bg=play_window["bg"], font=("Helvetica", 18))
+        list_of_letter_labels.append(label_of_letter)
+    current_column = 0
+    current_row = 1
+    for i, label_letter in enumerate(list_of_letter_labels):
+        if current_column >= 25 and list_of_letter_labels[i - 1].cget("text") == ' ':
+            current_row += 1
+            current_column = 0
+        label_letter.grid(row=current_row, column=current_column)
+        current_column += 1
+    return list_of_letter_labels
+
--- UI/welcome_window.py
+++ UI/welcome_window.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import keyboard
+import UI.play_window
 import UI.login
 from easygui import fileopenbox
 import time
@@ -9,50 +9,26 @@ letter_of_index = 0
 
 
 def start_game():
+    UI.play_window.initialize_play_window()
     # UI.login.create_login_window()
-    welcome_window = Tk()
-    define_window_style(welcome_window)
-
-
-    add_pick_file_widgets(welcome_window)
-    list_of_letter_labels = add_text_for_typing(welcome_window)
-    welcome_window.bind("<KeyPress-Shift_L>", lambda event: analyze_shift_press(event, list_of_letter_labels))
-    welcome_window.bind("<KeyPress-Shift_R>", lambda event: analyze_shift_press(event, list_of_letter_labels))
-    welcome_window.bind("<Key>", lambda event: analyze_key_press(event, list_of_letter_labels))
-
-    welcome_window.mainloop()
-    # start_typing(welcome_window, list_of_letter_labels)
+    #welcome_window = Tk()
+    #define_window_style(welcome_window)
+    # add_pick_file_widgets(welcome_window)
+    #welcome_window.mainloop()
 
 
 def define_window_style(welcome_window):
-    welcome_window.rowconfigure((0, 1, 2, 3, 4), weight=1)
-    welcome_window.columnconfigure((0, 1, 2, 3, 4), weight=1)
-    welcome_window.title("Type Master")
-    welcome_window.geometry("1000x500")
-    welcome_window["bg"] = "light blue"
-    Label(welcome_window, text="Type!", bg="light blue", font=("Helvetica", 30)).place(x=440, y=0)
+    pass
 
 
-def analyze_shift_press(event, letter_labels):
-    while keyboard.is_pressed("shift"):
-        if keyboard.read_key():
-            break
 
-
-def analyze_key_press(event, letter_labels):
-    global letter_of_index
-    if event.char == letter_labels[letter_of_index].cget("text"):
-        letter_labels[letter_of_index]["bg"] = "lawn green"
-    else:
-        letter_labels[letter_of_index]["bg"] = "tomato"
-    letter_of_index += 1
-
-
-def add_pick_file_widgets(welcome_window):
-    pick_file_label = Label(welcome_window, text="Pick a text file to practice typing from", bg=welcome_window["bg"], font=("Helvetica", 15))
-    pick_file_label.place(relx=0.05, rely=0.3)
-    pick_file_button = Button(welcome_window, text="Choose Text File", command=lambda: pick_file(welcome_window))
-    pick_file_button.place(relx=0.4, rely=0.305)
+#
+#
+# def add_pick_file_widgets(welcome_window):
+#     pick_file_label = Label(welcome_window, text="Pick a text file to practice typing from", bg=welcome_window["bg"], font=("Helvetica", 15))
+#     pick_file_label.place(relx=0.05, rely=0.3)
+#     pick_file_button = Button(welcome_window, text="Choose Text File", command=lambda: pick_file(welcome_window))
+#     pick_file_button.place(relx=0.4, rely=0.305)
 
 
 def pick_file(welcome_window):
@@ -68,23 +44,7 @@ def pick_file(welcome_window):
         pass
 
 
-def add_text_for_typing(welcome_window):
-    frame = Frame(welcome_window, width=950, height=430, bg="light blue")
-    frame.grid(row=1, column=0)
-    text = "Hello my name is Eldar, and this is my typing game. I hope you enjoy it."
-    list_of_letter_labels = []
-    for letter in text:
-        label_of_letter = Label(frame, text=letter, bg=welcome_window["bg"], font=("Helvetica", 18))
-        list_of_letter_labels.append(label_of_letter)
-    current_column = 0
-    current_row = 1
-    for i, label_letter in enumerate(list_of_letter_labels):
-        if current_column >= 25 and list_of_letter_labels[i - 1].cget("text") == ' ':
-            current_row += 1
-            current_column = 0
-        label_letter.grid(row=current_row, column=current_column)
-        current_column += 1
-    return list_of_letter_labels
+
 
 
 
